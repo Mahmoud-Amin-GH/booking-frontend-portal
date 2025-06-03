3b951b3b1de1f3ef29639dfd99fb12ea35d4f3bb
Mahmoud Amin <128816526+Mahmoud-Amin-GH@users.noreply.github.com>
2025-06-04 00:55:19 +0300

    remove duplicate design system files

--- design_system/Loader.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Loader: React.FC = () => (
-  <span className="inline-block w-5 h-5 border-2 border-blue-500 border-t-transparent rounded-full animate-spin" />
-);
-
-export default Loader; 
